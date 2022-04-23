ee71c96371d38d369ee7fccc82698ab56eb4015b
Amol Dhamale <amoldhamale1105@gmail.com>
2022-04-23 14:40:48 +0530

    downloading, pruning and balancing collected track data

---
+++ BehaviouralCloning.py
@@ -0,0 +1,88 @@
+!git clone https://github.com/amoldhamale1105/AutonomousCarTrackData.git
+
+!ls AutonomousCarTrackData
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from keras.models import Sequential
+from keras.models import Model
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers import Dropout
+from keras.optimizers import adam_v2
+from keras.utils.np_utils import to_categorical
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import cv2
+import pandas as pd
+import ntpath
+import random
+
+datadir = 'AutonomousCarTrackData'
+columns = ['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed']
+data = pd.read_csv(os.path.join(datadir, 'driving_log.csv'), names = columns)
+pd.set_option('display.max_colwidth', None)
+data.head()
+
+def path_leaf(path):
+  head, tail = ntpath.split(path)
+  return tail
+data['center'] = data['center'].apply(path_leaf)
+data['left'] = data['left'].apply(path_leaf)
+data['right'] = data['right'].apply(path_leaf)
+data.head()
+
+num_bins = 25
+samples_per_bin = 200
+hist, bins = np.histogram(data['steering'], num_bins)
+center = (bins[:-1] + bins[1:]) * 0.5 #Make the data zero centered using adjacent element wise addition
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(data['steering'])), (samples_per_bin, samples_per_bin))
+
+print('total data:', len(data))
+remove_list = []
+for j in range(num_bins):
+  list_ = []
+  for i in range(len(data['steering'])):
+    if data['steering'][i] >= bins[j] and data['steering'][i] <= bins[j+1]:
+      list_.append(i)
+  list_ = shuffle(list_)
+  list_ = list_[samples_per_bin:]
+  remove_list.extend(list_)
+
+print('removed:', len(remove_list))
+data.drop(data.index[remove_list], inplace=True)
+print('remaining:', len(data))
+
+hist, _ = np.histogram(data['steering'], (num_bins))
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(data['steering'])), (samples_per_bin, samples_per_bin))
+
+print(data.iloc[1])
+def load_img_steering(datadir, df):
+  image_path = []
+  steering = []
+  for i in range(len(data)):
+    indexed_data = data.iloc[i]
+    center, left, right = indexed_data[0], indexed_data[1], indexed_data[2]
+    image_path.append(os.path.join(datadir, center.strip()))
+    steering.append(float(indexed_data[3]))
+  image_paths = np.asarray(image_path)
+  steerings = np.asarray(steering)
+  return image_paths, steerings
+
+image_paths, steerings = load_img_steering(datadir + '/IMG', data)
+
+X_train, X_val, y_train, y_val = train_test_split(image_paths, steerings, test_size=0.2, random_state=6)
+print('Training samples: {}\nValidation Samples: {}'.format(len(X_train), len(X_val)))
+
+fig, axes = plt.subplots(1, 2, figsize=(12,4))
+axes[0].hist(y_train, bins=num_bins, width=0.05, color='blue')
+axes[0].set_title('Training set')
+axes[1].hist(y_val, bins=num_bins, width=0.05, color='red')
+axes[1].set_title('Validation set')
+
